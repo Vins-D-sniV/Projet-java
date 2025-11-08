7a1ec6a6da64045f5466cccc27a71e25dd587ac6
Vincent <vincent.nze@etu.univ-amu.fr>
2025-11-08 19:00:55 +0100

    initial

---
+++ projet-api/src/main/java/com/projetSpring/api/ProjetApiApplication.java
@@ -0,0 +1,12 @@
+package com.projetSpring.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetApiApplication.class, args);
+    }
+}
